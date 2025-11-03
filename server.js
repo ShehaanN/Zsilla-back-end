31cfa3b669e157b2004a8b87179fbc6a962e8d6f
ShehaanN <shehannadeesha2580@gmail.com>
2025-11-03 13:53:21 +0530

    feat: enable CORS support for API requests

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ import express from "express";
 import dotenv from "dotenv/config";
 import rootRouter from "./src/api/index.js";
 import connectDB from "./src/infrastructure/db/index.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-connectDB();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api", rootRouter);
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
